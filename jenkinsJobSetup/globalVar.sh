ab73786ab1b8115009611815226128af08be20cb
Kai.Zhou <zhoukaisspu@163.com>
2020-05-22 07:22:57 -0400

    Rename the name for cppscan repo in host

--- jenkinsJobSetup/globalVar.sh
+++ jenkinsJobSetup/globalVar.sh
@@ -2,5 +2,5 @@
 # Define global variables used in jenkins job setup related
 
 export DEPLOY_DIR_BASE="${HOME}/jenkinsJob"
-export CPPSCAN_DIR="${DEPLOY_DIR_BASE}/cppScan"
+export CPPSCAN_DIR="${DEPLOY_DIR_BASE}/cppscan.git"
 export CPP_SCAN_REPO="https://github.com/zhoukaisspu/job.cppscan.git"
