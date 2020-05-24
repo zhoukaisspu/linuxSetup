e082effde0358871910c12f130d2d920ab6671af
Kai.Zhou <zhoukaisspu@163.com>
2020-05-24 03:52:17 -0400

    Add necessary steps for cppscan repo setup

---
+++ jenkinsJobSetup/cppscanSetup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# It will clone the automation job from remote server and
+# deploy these into host computer.
+# It can imporve the jenkins performance, since jenkins have to
+# clone the job and execute it.
+
+source ../common/common.sh
+source ./globalVar.sh
+
+CURRENT_DIR=$(getScriptAbsolutePath)
+echo ${CURRENT_DIR}
+printlog "Start to remove cppscan repo from host"
+if [[ -d "${CPPSCAN_DIR}" ]]; then
+	rm -R "${CPPSCAN_DIR}"
+fi
+checkExitCode $? "rm cppscan folder"
+printlog "Remove ${CPPSCAN_DIR} successfully"
+
+printlog "Start to remove cppscan source folder from host"
+if [[ -d "${CPPSCAN_SOURCE_DIR}" ]]; then
+	rm -R "${CPPSCAN_SOURCE_DIR}"
+fi
+checkExitCode $? "rm cppscan folder"
+printlog "Remove ${CPPSCAN_SOURCE_DIR} successfully"
+
+printlog "Start to create ${CPPSCAN_DIR} folder"
+mkdir -p "${CPPSCAN_DIR}"
+checkExitCode $? "create ${CPPSCAN_DIR} folder"
+printlog "Create ${CPPSCAN_DIR} successfully"
+
+printlog "Start to create ${CPPSCAN_SOURCE_DIR} folder"
+mkdir -p "${CPPSCAN_SOURCE_DIR}"
+checkExitCode $? "create ${CPPSCAN_SOURCE_DIR} folder"
+printlog "Create ${CPPSCAN_SOURCE_DIR} successfully"
+
+
+printlog "Start to init cppscan bare repo"
+cd "${CPPSCAN_DIR}"
+git init --bare
+checkExitCode $? "init cppscan bare repo"
+printlog "Init cppscan bare repo successfully"
+
+printlog "Start to clone cppscan from remote server"
+cd "${CPPSCAN_SOURCE_DIR}"
+git clone "${CPPSCAN_REMOTE_REPO}" .
+checkExitCode $? "clone cppscan repo"
+printlog "clone cppscan repo successfully"
+
+printlog "Start to push cppscan to local bare repo"
+git remote add "${LOCAL_REPO_NAME}" "${CPPSCAN_SOURCE_DIR}"
+git push "${LOCAL_REPO_NAME}" master
+printlog "Push cppscan to local repo successfully"
+
--- jenkinsJobSetup/globalVar.sh
+++ jenkinsJobSetup/globalVar.sh
@@ -2,5 +2,8 @@
 # Define global variables used in jenkins job setup related
 
 export DEPLOY_DIR_BASE="${HOME}/jenkinsJob"
+export LOCAL_REPO_NAME="localRepo"
 export CPPSCAN_DIR="${DEPLOY_DIR_BASE}/cppscan.git"
-export CPP_SCAN_REPO="https://github.com/zhoukaisspu/job.cppscan.git"
+export CPPSCAN_SOURCE_DIR="${DEPLOY_DIR_BASE}/cppscan"
+#export CPPSCAN_REMOTE_REPO="https://github.com/zhoukaisspu/job.cppscan.git"
+export CPPSCAN_REMOTE_REPO="https://github.com/zhoukaisspu/linuxSetup.git"
