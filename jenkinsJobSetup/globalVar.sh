9253cf6a772de018a4813173b93d97e9ca6764a2
Kai Zhou <zhoukaisspu@163.com>
2020-06-23 21:44:56 +0800

    Change the DEPLOY_DIR_BASE

--- jenkinsJobSetup/globalVar.sh
+++ jenkinsJobSetup/globalVar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Define global variables used in jenkins job setup related
 
-export DEPLOY_DIR_BASE="${HOME}/jenkinsJob"
+export DEPLOY_DIR_BASE="/var/tmp/myJenkinsJob"
 export LOCAL_REPO_NAME="localRepo"
 
 export CPPSCAN_DIR="${DEPLOY_DIR_BASE}/job.cppscan.git"
