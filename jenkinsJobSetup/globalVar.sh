20e5912c2af0fd73d9353ba1db2ace20675389e7
zhou,kai <kai.zhou@microfocus.com>
2020-06-28 13:46:41 +0800

    Update the order of variable

--- jenkinsJobSetup/globalVar.sh
+++ jenkinsJobSetup/globalVar.sh
@@ -4,14 +4,14 @@
 export DEPLOY_DIR_BASE="/var/tmp/myJenkinsJob"
 export LOCAL_REPO_NAME="localRepo"
 
+export CPPSCAN_REMOTE_REPO="https://${githubUserName}:${githubToken}@github.com/zhoukaisspu/job.cppscan.git"
 export CPPSCAN_DIR="${DEPLOY_DIR_BASE}/job.cppscan.git"
 export CPPSCAN_SOURCE_DIR="${DEPLOY_DIR_BASE}/job.cppscan"
-export CPPSCAN_REMOTE_REPO="https://${githubUserName}:${githubToken}@github.com/zhoukaisspu/job.cppscan.git"
 
+export DB_CPPSCAN_REMOTE_REPO="https://${githubUserName}:${githubToken}@github.com/zhoukaisspu/db.cppscan.git"
 export DB_CPPSCAN_DIR="${DEPLOY_DIR_BASE}/db.cppscan.git"
 export DB_CPPSCAN_SOURCE_DIR="${DEPLOY_DIR_BASE}/db.cppscan"
-export DB_CPPSCAN_REMOTE_REPO="https://${githubUserName}:${githubToken}@github.com/zhoukaisspu/db.cppscan.git"
 
+export KWS_REPO="https://${githubUserName}:${githubToken}@github.com/xwJohn/KWS.git"
 export KWS_DIR="${DEPLOY_DIR_BASE}/KWS.git"
 export KWS_SOURCE_DIR="${DEPLOY_DIR_BASE}/KWS"
-export KWS_REPO="https://${githubUserName}:${githubToken}@github.com/xwJohn/KWS.git"
